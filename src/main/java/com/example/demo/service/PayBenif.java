2c3db9d94ca223113939e5d1248883c3aa9a9fbe
한철 <chul.han@biplatform.co.kr>
2024-11-21 18:09:24 +0900

    커밋합니다.

---
+++ src/main/java/com/example/demo/service/PayBenif.java
@@ -0,0 +1,6 @@
+package com.example.demo.service;
+
+public class PayBenif {
+
+    //배팅에 대한 배당을 확인
+}
---
+++ src/main/java/com/example/demo/service/ShutDownService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+import java.util.Map;
+
+public class ShutDownService {
+    
+    //기간안에 횟수와 금액을 확인
+    public Map<String, Object> chkCntAmt(Map<String, Object> ParamMap){
+        return ParamMap;
+    }
+}
